005ae35e68b60de73caa2892b1613e1bf4f2ba2a
Fawzi Alami <fawzi.alami@gmail.com>
2024-01-09 19:55:46 -0600

    add words.json file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const app = require('express')();
+const words = require('./words.json');
 
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req, res) => {
-    res.send("Hello world!");
+    res.send(`There are ${words.length} entries in the dictionary!`);
 });
 
 app.listen(PORT, () => {
