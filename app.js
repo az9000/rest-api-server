189bee827fac76ad8c6449e40a8a3d291d7424c7
Fawzi Alami <fawzi.alami@gmail.com>
2024-01-09 20:27:39 -0600

    added code to generate 30 random objects

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const words = require('./words.json');
 
 const PORT = process.env.PORT || 3000;
 
+const MAX_WORDS = 30;
+
 app.get("", (req, res) => {
     res.send(`There are ${words.length} entries in the dictionary!`);
+});
+
+app.get("/words/r", (req, res) => {
+    // get a random number
+    var len = words.length;
+    var rnum = Math.floor(Math.random() * (len + 1) + 0);
+    var wordsArray = [];
+    wordsArray.unshift(words[rnum]);
+    for (var i=0; i < len; i++) {
+        rnum = Math.floor(Math.random() * (len + 1) + 0);
+        for (var j=0; j < wordsArray.length; j++) {
+            if (wordsArray[j]['id'] != words[rnum]['id']) {
+                wordsArray.unshift(words[rnum]);
+                
+                break;
+            }
+        }
+        if (wordsArray.length === MAX_WORDS) {
+            break;
+        }
+    }
+
+    res.send(wordsArray);
 });
 
 app.listen(PORT, () => {
