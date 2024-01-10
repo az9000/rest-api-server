99a4aa17d085e714512b93bec5d4cfe2fc1fa583
Fawzi Alami <fawzi.alami@gmail.com>
2024-01-09 20:42:05 -0600

    modified code to include flexible list size based on request

--- app.js
+++ app.js
@@ -6,26 +6,33 @@ const PORT = process.env.PORT || 3000;
 const MAX_WORDS = 30;
 
 app.get("", (req, res) => {
-    res.send(`There are ${words.length} entries in the dictionary!`);
+    res.send(`There are ${words.length} entries in the dictionary!\nUse /words/r to get 30 random objects!`);
 });
 
-app.get("/words/r", (req, res) => {
+app.get("/words/:max", (req, res) => {
+    // number of entries
+    var num = parseInt(req.params.max, 10);
+    if (num === 0) {
+        num = MAX_WORDS;
+    } 
     // get a random number
     var len = words.length;
     var rnum = Math.floor(Math.random() * (len + 1) + 0);
     var wordsArray = [];
     wordsArray.unshift(words[rnum]);
-    for (var i=0; i < len; i++) {
-        rnum = Math.floor(Math.random() * (len + 1) + 0);
-        for (var j=0; j < wordsArray.length; j++) {
-            if (wordsArray[j]['id'] != words[rnum]['id']) {
-                wordsArray.unshift(words[rnum]);
-                
+    if (num > 1) {
+        for (var i=0; i < len; i++) {
+            rnum = Math.floor(Math.random() * (len + 1) + 0);
+            for (var j=0; j < wordsArray.length; j++) {
+                if (wordsArray[j]['id'] != words[rnum]['id']) {
+                    wordsArray.unshift(words[rnum]);
+                    
+                    break;
+                }
+            }
+            if (wordsArray.length === num) {
                 break;
             }
-        }
-        if (wordsArray.length === MAX_WORDS) {
-            break;
         }
     }
 
