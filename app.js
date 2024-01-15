319040a6c48b45d418b7de59af0b4d9e50aa87c8
Fawzi Alami <fawzi.alami@gmail.com>
2024-01-15 07:27:50 -0600

    updated words.json

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 const MAX_WORDS = 30;
 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 app.get("", (req, res) => {
     res.send(`There are ${words.length} entries in the dictionary!\nUse /words/r to get 30 random objects!`);
 });
@@ -14,19 +20,19 @@ app.get("/words/:max", (req, res) => {
     var num = parseInt(req.params.max, 10);
     if (num === 0) {
         num = MAX_WORDS;
-    } 
+    }
     // get a random number
     var len = words.length;
-    var rnum = Math.floor(Math.random() * (len + 1) + 0);
+    var rnum = getRandomIntInclusive(0, len);
     var wordsArray = [];
     wordsArray.unshift(words[rnum]);
     if (num > 1) {
-        for (var i=0; i < len; i++) {
-            rnum = Math.floor(Math.random() * (len + 1) + 0);
-            for (var j=0; j < wordsArray.length; j++) {
+        for (var i = 0; i < len; i++) {
+            rnum = getRandomIntInclusive(0, len);
+            for (var j = 0; j < wordsArray.length; j++) {
                 if (wordsArray[j]['id'] != words[rnum]['id']) {
                     wordsArray.unshift(words[rnum]);
-                    
+
                     break;
                 }
             }
@@ -40,5 +46,5 @@ app.get("/words/:max", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`App up at PORT ${PORT}`);
+    console.log(`App up at http://localhost:${PORT}`);
 });
